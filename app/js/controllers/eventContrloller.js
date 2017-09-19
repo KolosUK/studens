2e04a8590da5bed469474d3e167e3478b8e84ada
Pavlo Didyk <pavlo.didyk@neadevis.com>
2017-09-19 15:26:43 +0300

    Add main script in file

---
+++ app/js/controllers/eventContrloller.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+eventsApp.controller('EventController',
+function EventController($scope) {
+
+    $scope.event = {
+        name: 'Angular Boot Camp',
+        date: '19/09/2017',
+        time: '10:00',
+        location: {
+            address: 'Frosty water',
+            city: 'Lviv',
+            province: 'UA'
+        },
+
+        imageURL: '/img/angularjs-logo.png'
+    }
+
+}
+);
